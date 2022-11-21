86be2d0d17e47c70bf3f9441c0fb55ecc926a684
Harsh <harshpachani@gmail.com>
2022-11-21 21:39:31 +0530

    Initial commit

---
+++ Textutils/Textutils/Views.py
@@ -0,0 +1,3 @@
+#I have created this file - Harsh
+def index(request): #the django is automatically passing an argument call request, so we have to give that argument to our python function
+    return "hello"
