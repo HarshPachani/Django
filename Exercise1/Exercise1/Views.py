7ad9d025a16a2b4d4478c4c89ce2fd89bc3833fa
Harsh <harshpachani@gmail.com>
2022-11-22 17:58:28 +0530

    Exercise1

--- Exercise1/Exercise1/Views.py
+++ Exercise1/Exercise1/Views.py
@@ -2,4 +2,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Harsh</h1>")
+    return HttpResponse("""<h1>Harsh</h1> <a href = "https://www.youtube.com/watch?v=5BDgKJFZMl8&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9"> Django CodeWithHarry </a>""")
